06962367a12ea362b30c7c068ecb47500b043874
Tomek Felisiak <tomekfel@googlemail.com>
2018-06-04 19:55:52 +0100

    db setup

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const monk = require('monk');
-const connectionString = process.env.MONGODB_URI || 'localhost/messageboard';
+const connectionString = process.env.PROD_MONGODB || 'localhost/messageboard';
 const db = monk(connectionString);
 
 module.exports = db;
